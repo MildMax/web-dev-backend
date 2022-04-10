8dda5d8d4e53f0e94ae95dffdb13d7fa6c22206b
MildMax <joseph.s.burns93@gmail.com>
2022-04-10 10:48:40 -0400

    fixed env var passing to match updated assignment spec

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 
-const username = process.env.MONGO_USER;
-const password = process.env.MONGO_PW;
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev';
 
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.daxtl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
